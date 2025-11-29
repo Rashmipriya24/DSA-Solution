8d59c78af3deedfeffc7c3a46d9804c04f3cc825
Rashmipriya24 <168108359+Rashmipriya24@users.noreply.github.com>
2025-11-29 21:42:17 +0530

    Add files via upload

---
+++ kth_permutation.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string getpermutation(int n,int k){
+    int fact = 1;
+    vector<int>number;
+    for(int i = 1;i<n;i++){
+        fact= fact*i;
+        number.push_back(i);
+    }
+    number.push_back(n);
+    string ans ="";
+    k = k-1;
+    while(true){
+        ans = ans + to_string(number[k/fact]);
+        number.erase(number.begin()+k/fact);
+        if(number.size()==0){
+            break;
+        }
+        k = k%fact;
+        fact = fact/number.size();
+    }
+    return ans;
+
+}
+
+int main(){
+    int n;
+    int k;
+    cin>>n;
+    cin>>k;
+    string result = getpermutation(n,k);
+    cout<<result;
+}
