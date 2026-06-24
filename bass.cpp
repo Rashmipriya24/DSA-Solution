92676f24c3ec8374f9ef860695bbcf43dfebac9e
Rashmipriya24 <168108359+Rashmipriya24@users.noreply.github.com>
2026-06-24 14:54:25 +0530

    Add files via upload

---
+++ bass.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i = 0;i<n;i++){
+        cin>>arr[i];
+    }
+    int mini =arr[0];
+    int profit = 0;
+    for(int i =1;i<n;i++){
+        mini = min(mini,arr[i]);
+        profit = max(profit,arr[i]-mini);
+    }
+    cout<<profit<<endl;
+    return 0;
+}
