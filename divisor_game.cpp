74af35c7d8466e2470242b26956c3058fbbb0ffb
Rashmipriya24 <168108359+Rashmipriya24@users.noreply.github.com>
2026-07-08 13:53:03 +0530

    Add files via upload

---
+++ divisor_game.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<bool>dp(n+1,false);
+    dp[1] = false;
+    for(int i = 2;i<=n;i++){
+        for(int x=1;x<i;x++){
+            if((i%x)==0&&dp[i-x]==false){
+                dp[i]=true;
+                break;
+            }
+           
+        }
+    }
+    cout<<boolalpha<<dp[n];
+    return 0;
+
+}
