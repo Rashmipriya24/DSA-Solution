fbfda2601c5597d80fe504aebfce52b5ff798739
Rashmipriya24 <168108359+Rashmipriya24@users.noreply.github.com>
2026-07-19 15:50:02 +0530

    Add files via upload

---
+++ valid_parenthesis.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isValid(string s) {
+        stack<char>st;
+        for(char ch:s){
+            if(ch=='('||ch=='{'||ch=='['){
+                st.push(ch);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                char top=st.top();
+                st.pop();
+                if(ch==')'&&top!='('){
+                    return false;
+                }
+                if(ch=='}'&&top!='{'){
+                    return false;
+                }
+                if(ch==']'&&top!='['){
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+        
+    }
+
+int main(){
+    string s;
+    cin>>s;
+    if(isValid(s)){
+        cout<<"true";
+    }
+    else{
+        cout<<"false";
+
+    }
+    return 0;
+
+}
